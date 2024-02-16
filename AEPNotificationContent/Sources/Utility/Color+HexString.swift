8170a7d45baf360ce6f7586bf7b88988c8c79f0e
Pravin Prakash Kumar <pprakash@Pravins-MacBook-Pro.local>
2024-02-16 12:12:30 -0800

    Correct the hex conversion code

--- AEPNotificationContent/Sources/Utility/Color+HexString.swift
+++ AEPNotificationContent/Sources/Utility/Color+HexString.swift
@@ -13,7 +13,7 @@
 import UIKit
 
 extension UIColor {
-
+    
     convenience init?(hexString: String) {
         var hexString: String = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
         hexString = hexString.replacingOccurrences(of: "#", with: "")
@@ -22,21 +22,38 @@ extension UIColor {
         guard hexString.count == 6 || hexString.count == 8 else {
             return nil
         }
+        
+        // Check for valid hexadecimal characters
+        let validHexChars = CharacterSet(charactersIn: "0123456789ABCDEFabcdef")
+        guard CharacterSet(charactersIn: hexString).isSubset(of: validHexChars) else {
+            return nil
+        }
 
         // Create scanner for hex string
         var color: UInt64 = 0
         let scanner = Scanner(string: hexString)
         scanner.scanHexInt64(&color)
 
+        // bit shifting for picking the correct component
         let mask = 0x000000FF
-        let r = Int(color >> 16) & mask
-        let g = Int(color >> 8) & mask
-        let b = Int(color) & mask
-
-        let red   = CGFloat(r) / 255.0
+         var r, g, b, a: Int
+         if hexString.count == 8 {
+             // 8-digit hex (including alpha)
+             r = Int(color >> 24) & mask
+             g = Int(color >> 16) & mask
+             b = Int(color >> 8) & mask
+             a = Int(color) & mask
+         } else {
+             // 6-digit hex
+             a = 255
+             r = Int(color >> 16) & mask
+             g = Int(color >> 8) & mask
+             b = Int(color) & mask
+         }
+        let red = CGFloat(r) / 255.0
         let green = CGFloat(g) / 255.0
-        let blue  = CGFloat(b) / 255.0
-        let alpha = (hexString.count == 8) ? CGFloat(Int(color >> 24) & mask) / 255.0 : 1.0
+        let blue = CGFloat(b) / 255.0
+        let alpha = CGFloat(a) / 255.0
 
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
