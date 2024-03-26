8f741e8188941d8115da6117a659764505444a66
Steve Benedick <sbenedic@adobe.com>
2024-03-26 16:46:32 -0600

    -fix the string extension

--- AEPNotificationContent/Sources/Utility/String+LabelHeight.swift
+++ AEPNotificationContent/Sources/Utility/String+LabelHeight.swift
@@ -17,7 +17,7 @@ import UIKit
 extension String {
     func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
-        let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(boundingBox.height)
     }
 }
