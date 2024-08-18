da026d2a1f777b3b9a4e71dfb82acc9c0624e9bc
Pravin Prakash Kumar <pravinpk0707@gmail.com>
2024-08-18 01:13:44 -0700

    Swift format

--- Frameworks/AEPSwiftUI/Sources/ContentCards/Template/BaseTemplate.swift
+++ Frameworks/AEPSwiftUI/Sources/ContentCards/Template/BaseTemplate.swift
@@ -3,15 +3,15 @@
  This file is licensed to you under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License. You may obtain a copy
  of the License at http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software distributed under
  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  OF ANY KIND, either express or implied. See the License for the specific language
  governing permissions and limitations under the License.
  */
 
-import SwiftUI
 import AEPMessaging
+import SwiftUI
 
 /// A base class for content card templates, providing common properties and initialization logic.
 ///
@@ -24,22 +24,19 @@ import AEPMessaging
 /// - Note: This class is not intended to be used directly. Instead, use one of its subclasses
 ///   that provide specific template implementations.
 public class BaseTemplate {
-    
     /// The background color of the content card.
     /// Use this property to set the background color for the content card.
     public var backgroundColor: Color?
-    
+
     /// The URL that is intended to be opened when the content card is interacted with.
     var actionURL: URL?
-    
-    
+
     /// Initializes a `BaseTemplate` with the given schema data.
     /// This initializer is designed to be called by subclasses to perform common initialization tasks.
     /// - Parameter data: The schema data used for initialization.
     init?(_ data: ContentCardSchemaData) {
         self.actionURL = data.actionUrl
-        
-        // TODO : Retrieve other common properties for all templated content cards
+
+        // TODO: Retrieve other common properties for all templated content cards
     }
 }
-
--- Frameworks/AEPSwiftUI/Sources/ContentCards/Template/SmallImageTemplate.swift
+++ Frameworks/AEPSwiftUI/Sources/ContentCards/Template/SmallImageTemplate.swift
@@ -3,15 +3,15 @@
  This file is licensed to you under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License. You may obtain a copy
  of the License at http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software distributed under
  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  OF ANY KIND, either express or implied. See the License for the specific language
  governing permissions and limitations under the License.
  */
 
-import SwiftUI
 import AEPMessaging
+import SwiftUI
 
 /// A class representing a content card template with a small image layout.
 ///
@@ -21,35 +21,33 @@ import AEPMessaging
 /// If the required `title` is missing from the schema data, the initialization fails.
 ///
 /// - Note: The `view` property is lazily initialized and represents the entire layout of the content card.
-public class SmallImageTemplate : BaseTemplate, ContentCardTemplate {
+public class SmallImageTemplate: BaseTemplate, ContentCardTemplate {
     var templateType: ContentCardTemplateType = .smallImage
-    
+
     /// The title of the content card.
-    var title : AEPText
-    
+    var title: AEPText
+
     /// The body text of the content card, if available.
-    var body : AEPText?
-    
+    var body: AEPText?
+
     /// The image associated with the content card, if available.
-    var image : AEPImage?
-    
+    var image: AEPImage?
+
     /// The buttons associated with the content card, if available.
-    var buttons : [AEPButton]?
-    
+    var buttons: [AEPButton]?
+
     /// A horizontal stack for arranging buttons.
     var buttonHStack = AEPHStack()
-    
+
     /// A vertical stack for arranging the title, body, and buttons.
     var textVStack = AEPVStack()
-    
+
     /// A horizontal stack for arranging the image and text stack.
     var rootHStack = AEPHStack()
-    
+
     /// The SwiftUI view representing the entire layout of the content card.
-    public lazy var view: some View = {
-        rootHStack.view
-    }()
-    
+    public lazy var view: some View = rootHStack.view
+
     /// Initializes a `SmallImageTemplate` with the given schema data.
     ///
     /// This initializer extracts the title, body, image, and buttons from the provided `ContentCardSchemaData`.
@@ -57,7 +55,7 @@ public class SmallImageTemplate : BaseTemplate, ContentCardTemplate {
     ///
     /// - Parameter data: The schema data used to populate the template's properties.
     /// - Returns: An initialized `SmallImageTemplate` or `nil` if the required title is missing.
-    override init?(_ data : ContentCardSchemaData) {
+    override init?(_ data: ContentCardSchemaData) {
         guard let title = data.title else {
             return nil
         }
@@ -65,23 +63,21 @@ public class SmallImageTemplate : BaseTemplate, ContentCardTemplate {
         self.body = data.body
         self.image = data.image
         self.buttons = data.buttons
-        
-        
+
         // Add buttons to buttonHStack
         if let buttons = buttons {
             for button in buttons {
                 buttonHStack.addModel(button)
             }
         }
-        
+
         // Add title, body, and buttonHStack to textVStack
         textVStack.addModel(title)
         if let body = body {
             textVStack.addModel(body)
         }
         textVStack.addModel(buttonHStack)
-        
-        
+
         // Add image and textVStack to rootHStack
         if let image = image {
             rootHStack.addModel(image)
